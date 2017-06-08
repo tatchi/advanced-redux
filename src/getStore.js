ff2ba3a45e5024a683ede4940e8baed70e30de52
Corentin Leruth <clt@pulsar.be>
2017-06-08 21:42:32 +0200

    Advanced immutability

--- src/getStore.js
+++ src/getStore.js
@@ -1,9 +1,17 @@
-import {
-    createStore
-} from 'redux';
+import { createStore } from 'redux';
+import { users } from '../server/db';
+import { initializeDB } from '../server/db/initializeDB';
+import { getDefaultState } from '../server/getDefaultState';
+
+initializeDB();
+
+const currentUser = users[0];
+const defaultState = getDefaultState(currentUser);
+
+console.log(defaultState);
 
 const reducer = state => state;
 
-const store = createStore(reducer);
+const store = createStore(reducer, defaultState);
 
-export const getStore = ()=>store;
+export const getStore = () => store;
